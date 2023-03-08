e40ce369c53848e99625e9bdadf51382253db5d8
Austin Asoluka <stracool9@gmail.com>
2023-03-08 17:25:54 +0100

    fix(google-auth): redirect on login success

--- src/components/pages/LoginPage/index.jsx
+++ src/components/pages/LoginPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Flex } from "../../atoms";
 import jwtDecode from "jwt-decode";
@@ -8,24 +8,35 @@ import {
   setUser,
 } from "../../../store/features/auth/authSlice";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { StatisticsTemplate } from "../../templates";
+import { Footer, NavBar } from "../../molecules";
+import { screen } from "../../theme/utils";
+import { useNavigate } from "react-router";
 
 const Wrapper = styled(Flex)`
   justify-content: center;
   padding-top: 20px;
-  height: 100vh;
+  min-height: 81vh;
 `;
 
 const SignOutBtn = styled.button`
   padding: 12px 32px;
   cursor: pointer;
   height: fit-content;
+  width: fit-content;
   border-radius: 64px;
   background-color: white;
+  border: 1px solid #eeeeee;
+
+  @media only screen and (${screen.sm}) {
+    width: 100%;
+  }
 `;
 
 export const LoginPage = () => {
   const user = useSelector(selectUserData);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSignout() {
     dispatch(setUser({}));
@@ -35,27 +46,27 @@ export const LoginPage = () => {
   async function handleResponse(response) {
     let userObject = jwtDecode(response.credential);
     await dispatch(setUser(userObject));
-    // navigate("/validators");
+    navigate("/validators");
   }
 
-  useEffect(() => {
-    handleSignout();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
-    <Wrapper>
-      {Object.keys(user).length !== 0 ? (
-        <SignOutBtn onClick={handleSignout}>Signout</SignOutBtn>
-      ) : (
-        <GoogleLogin
-          onSuccess={handleResponse}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-          useOneTap
-        />
-      )}
-    </Wrapper>
+    <StatisticsTemplate
+      header={<NavBar justifyContent="center" />}
+      footer={<Footer />}
+    >
+      <Wrapper>
+        {Object.keys(user).length !== 0 ? (
+          <SignOutBtn onClick={handleSignout}>Sign out</SignOutBtn>
+        ) : (
+          <GoogleLogin
+            onSuccess={handleResponse}
+            onError={(errorResponse) => {
+              console.log(errorResponse.error);
+            }}
+            useOneTap
+          />
+        )}
+      </Wrapper>
+    </StatisticsTemplate>
   );
 };
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -10,7 +10,6 @@ const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  blacklist: ["profile"],
 };
 
 const rootReducers = combineReducers({
