7eee06615cde5d8a940fb9360947fc89e1d9e130
Austin Asoluka <stracool9@gmail.com>
2023-03-09 21:28:12 +0100

    render only unique toasts

--- src/api-utils/globalAxios.js
+++ src/api-utils/globalAxios.js
@@ -15,18 +15,27 @@ globalAxios.interceptors.response.use(
   },
   (error) => {
     if (error.response.status === 419) {
-      toast.success("😎 Session expired!!!");
+      toast.success("😎 Session expired!!!", {
+        toastId: error.response.status,
+      });
     }
     if (error.response.status === 503) {
       toast.error(
-        "we are currently upgrading/improving our setup... try again shortly"
+        "we are currently upgrading/improving our setup... try again shortly",
+        {
+          toastId: error.response.status,
+        }
       );
     }
     if (error.response.status === 422) {
-      toast.error("Form validation error. Please cross-check your inputs");
+      toast.error("Form validation error. Please cross-check your inputs", {
+        toastId: error.response.status,
+      });
     }
     if (error.response.status === 429) {
-      toast.error("Take a deep breathe... 🥵 too many requests in a minute");
+      toast.error("Take a deep breathe... 🥵 too many requests in a minute", {
+        toastId: error.response.status,
+      });
     }
 
     return Promise.reject(error);
